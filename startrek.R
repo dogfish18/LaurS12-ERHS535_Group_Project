922bdbd9fee96bda3a7efedfb91b0fb64316436c
dogfish18 <94500384+dogfish18@users.noreply.github.com>
2021-12-13 23:34:17 -0700

    Upload final code for SE
    Sentiment analysis plots

--- startrek.R
+++ startrek.R
@@ -2,11 +2,6 @@
 library(tidyverse)
 library(tidytext)
 library(textdata)
-library(magrittr)
-library(reshape2)
-library(syuzhet)
-library(RColorBrewer)
-library(tm)
 library(viridis)
 
 startrek <- readr::read_csv(
@@ -27,50 +22,37 @@ startrek_tidy2 <- startrek %>%
                        'Picard (O.S.)' = "Picard", 'Picard (V.O.)' = "Picard", 'Young Picard' = "Picard",
                        'Jean-Luc' = "Picard")) 
 
-select_char <- c("Picard", "Geordi", "Data", "Riker", "Computer")
+select_char <- c("Picard", "Geordi", "Data", "Riker", "Computer", "Beverly")
 startrek_tidy3 <- filter(startrek_tidy2, char %in% select_char)
-
-##Load the lexicons
-
-sent_afinn <- get_sentiments("afinn") #score
-
-  
 
 ######Sentiment Analysis######
 
 ##bing
-sentiment_bing <- get_sentiments("bing")  #pos/neg
-
-bing_join <- sentiment_bing %>% 
+bing_join <- get_sentiments("bing") %>% 
   inner_join(startrek_tidy3, by = c("word" = "script_words")) %>% 
   group_by(sentiment, char) %>% 
   count()
 
-bing_summary <- bing_join %>% 
-  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
-  mutate(difference = positive - negative) 
-
 plot_bing <- bing_join %>% 
     ggplot(aes(char, sentiment, fill = sentiment, y = n)) +
     geom_bar(position = "stack", stat = "identity") +
-    xlab("Star Trek Character") +
-    ylab("Number of words") +
-    labs(title = "Word Sentiments in Star Trek TNG:", 
-         subtitle = "Character and Computer Interactions", 
+    xlab("Star Trek Character") + ylab("Number of words") +
+    labs(title = "TNG Character and Computer Interactions", 
+         subtitle = "Word Sentiment Analysis of the 'Bing' Lexicon", 
          caption = "Data source: www.tidytuesday.com") +
     theme(
       plot.title = element_text(size = 14, hjust = 0.5),
       plot.subtitle = element_text(size = 11, hjust = 0.5),
-      plot.caption = element_text(size = 9, hjust = 0.9),
+      plot.caption = element_text(size = 9, hjust = 0.5),
       legend.title = element_blank(),
       panel.grid.minor.x=element_blank(),
       panel.grid.major.x=element_blank(),
       axis.text.x = element_text(angle = 60, vjust = 0.5, hjust = 0.5),
       ) 
-  print(plot_bing)
+print(plot_bing)
 
 ###nrc (feelings)
-omit_sent <- c("positive", "negative")
+omit_sent <- c("positive", "negative") #take out data with pos/neg because already analyzed
 sent_nrc <- get_sentiments("nrc") %>% 
   filter(!sentiment %in% omit_sent)
   
@@ -82,15 +64,14 @@ nrc_join <- sent_nrc %>%
 plot_nrc <- nrc_join %>% 
   ggplot(aes(char, sentiment, fill = sentiment, y = n)) +
   geom_bar(position = "stack", stat = "identity") +
-  xlab("Star Trek Character") +
-  ylab("Number of words") +
-  labs(title = "Word Sentiments in Star Trek TNG:", 
-       subtitle = "Character and Computer Interactions", 
+  xlab("Star Trek Character") + ylab("Number of words") +
+  labs(title = "TNG Character and Computer Interactions", 
+       subtitle = "Word Sentiment Analysis of the 'NRC' Lexicon", 
        caption = "Data source: www.tidytuesday.com") +
   theme(
     plot.title = element_text(size = 14, hjust = 0.5),
     plot.subtitle = element_text(size = 11, hjust = 0.5),
-    plot.caption = element_text(size = 9, hjust = 0.9),
+    plot.caption = element_text(size = 8, hjust = 0.5),
     legend.title = element_blank(),
     panel.grid.minor.x=element_blank(),
     panel.grid.major.x=element_blank(),
@@ -98,21 +79,36 @@ plot_nrc <- nrc_join %>%
   ) 
 print(plot_nrc)
 
-
-
-startrek_afinn <- sent_afinn %>% 
-  inner_join(startrek_tidy2, by = c("word" = "script_words")) %>% 
+###afinn (score)
+sent_afinn <- get_sentiments("afinn") #score
+afinn_join <- sent_afinn %>% 
+  inner_join(startrek_tidy3, by = c("word" = "script_words")) %>% 
   group_by(value, char) %>% 
-  count() %>% 
-  pivot_wider(names_from = value, values_from = n, values_fill = 0)
+  count() 
 
+x_labels = c(-4:4)
+plot_afinn <- afinn_join %>% 
+  ggplot(aes(value, n, fill = value)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  scale_fill_viridis(option = "D") +
+  scale_color_viridis(option = "D") +
+  facet_wrap(vars(char)) +
+  xlab("Sentiment Score") +
+  ylab("Word Count") +
+  labs(title = "TNG Character and Computer Interactions", 
+       subtitle = "Word Sentiment Analysis of the 'Afinn' Lexicon", 
+       caption = "Data source: www.tidytuesday.com", 
+       color = "Word Count") +
+  theme(
+    plot.title = element_text(size = 14, hjust = 0.5),
+    plot.subtitle = element_text(size = 11, hjust = 0.5),
+    plot.caption = element_text(size = 9, hjust = 0.5),
+    panel.grid.minor.x=element_blank(),
+    panel.grid.major.x=element_blank(),
+    panel.grid.minor.y=element_blank(),
+    panel.grid.major.y=element_blank()) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_x_continuous(labels = x_labels, breaks = x_labels)
 
-#Comparison cloud
-library(reshape2)
-comp_cloud <- startrek_tidy2 %>%
-  group_by(char) %>%
-  count(script_word) %>%
-  acast(script_word ~ sentiment, value.var = "n", fill = 0) %>%
-  comparison.cloud(colors = c("gray20", "gray80"),
-                   max.words = 100)
+print(plot_afinn)
 
